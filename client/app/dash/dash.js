f1bed87139920335b967090412dbccff8d28a605
Femi Saliu <femisaliu.gmail.com>
2015-08-10 10:18:24 -0700

    [fix] Some small Materialize related fixes

--- client/app/dash/dash.js
+++ client/app/dash/dash.js
@@ -1,6 +1,9 @@
 angular.module('homeHarmony.dash',[])
 
 .controller('dashCtrl', function ($scope) {
-  $('.button-collapse').sideNav();
-  console.log('button-collapse enabled');
+  // Initializes Materialize Sidebar
+  $('.button-collapse').sideNav({
+    menuWidth: 200, // Default is 240
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 });
