d2a12a20c23023d860300da549ddf4992bd39607
Evan Mencke <metaevan@gmail.com>
2015-08-11 10:43:05 -0700

    [feat] starts implementation of tasks

--- client/app/tasks/tasks.js
+++ client/app/tasks/tasks.js
@@ -13,22 +13,27 @@ angular.module('homeHarmony.tasks', ['firebase'])
       dueDate: $scope.newTaskDueDate,
       dateCreated: new Date(),
       completed: false,
-      repeating = -1
+      repeating: -1
     }
     db.child('houses').child(currentHouseId).child('tasks').push(taskObj);
 
     db.child('houses').child(currentHouseId).child('tasks').on('child_added', function(snapshot){
       taskId = snapshot.key();
+      console.log(taskId);
       // get tasks
       $scope.taskArr.push(snapshot.val());
       console.log('tasks ', $scope.taskArr);
-    }
-
+    })
   };
 
   $scope.getTasks = function() {
     db.on('value', function(snapshot){
-      taskDb = snapshot.houses.currentHouseId.tasks;  //check later
+      var temp = snapshot.val()
+      console.log(temp, 'temp');
+      console.log(currentHouseId, 'CHID in getTasks');
+      var temp2= temp.houses[currentHouseId];
+      console.log('temp2 ', temp2)
+      taskDb = temp2.tasks;  //check later
       for (prop in taskDb) {
         $scope.taskArr.push(taskDb[prop]);
         console.log('gotten tasks ', $scope.taskArr)
