2a4077a3869267d69d6b31b4c692844e3a647841
Femi Saliu <femisaliu.gmail.com>
2015-08-14 12:37:28 -0700

    [feat] Issues show on the dashboard

--- client/app/dash/partials/defaultController.js
+++ client/app/dash/partials/defaultController.js
@@ -8,11 +8,15 @@ angular.module('homeHarmony.default', ['firebase'])
   var expensesDb;
   var expensesArr;
   var dataObj;
+  var issuesDb;
+  var issuesArr;
 
 
   db.once("value", function(snapshot) {
     expensesArr = [];
+    issuesArr = [];
     expensesDb = snapshot.val().houses[currentHouseId].expenses;
+    issuesDb = snapshot.val().houses[currentHouseId].issues;
     for (expense in expensesDb){
       dataObj = {};
       dataObj.name = expensesDb[expense].expenseName;
@@ -22,6 +26,14 @@ angular.module('homeHarmony.default', ['firebase'])
     $q.all(expensesArr).then(function(){
       $scope.expensesArr = expensesArr;
       DrawPie.drawPie($scope, "", false);
+    });
+
+    for (issue in issuesDb){
+      issuesArr.push(issuesDb[issue]);
+    }
+    $q.all(issuesArr).then(function(){
+      $scope.issuesArr = issuesArr;
+      console.log($scope.issuesArr);
     });
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
