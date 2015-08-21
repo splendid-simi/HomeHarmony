9ddefc1dab4978dc09c6749b072cb363501ca80d
KalisaFalzone <kalisa.falzone@gmail.com>
2015-08-21 14:24:27 -0700

    adds bower files for deployment

---
+++ client/lib/firebaseLink.js
@@ -0,0 +1,5 @@
+DB = {
+  url: 'https://homeharmony123abc.firebaseio.com/'
+  //semi url: 'https://homeharmonydsaokjsahdfkhasf234987140720505082305870dslfh.firebaseio.com/'
+  //url: "https://dazzling-inferno-3592.firebaseio.com"
+};
