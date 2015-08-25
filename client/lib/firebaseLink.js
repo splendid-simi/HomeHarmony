3a418c52baef2e18e2f4eebf0306235c4f154af6
KalisaFalzone <kalisa.falzone@gmail.com>
2015-08-24 19:34:44 -0700

    fixes DB merge confict

--- client/lib/firebaseLink.js
+++ client/lib/firebaseLink.js
@@ -1,10 +1,3 @@
 DB = {
-<<<<<<< HEAD
-  //url: 'https://homeharmony123abc.firebaseio.com/'
   url: 'https://homeharmonydsaokjsahdfkhasf234987140720505082305870dslfh.firebaseio.com/'
-  //url: "https://dazzling-inferno-3592.firebaseio.com"
-=======
-  url: 'https://homeharmonydsaokjsahdfkhasf234987140720505082305870dslfh.firebaseio.com/'
-  // this is the old url: "https://dazzling-inferno-3592.firebaseio.com"
->>>>>>> deploy
 };
